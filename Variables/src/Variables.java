74b48a94b34f1587540e5f6a4f23d69d21de967c
Igo <igorgasparsm@gmail.com>
2023-02-18 23:10:13 -0300

    testing commit github

---
+++ Variables/src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        // Increment
+        int a = 10;
+        a++;
+        System.out.println(a);
+
+        // O que aconteceu aqui?
+
+    }
+}
